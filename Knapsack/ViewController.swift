e3f1b938d10aa553770877f6c8df9fafb7dba979
elliotsjohnston <elliotsjohnston@icloud.com>
2022-12-08 21:17:04 -0800

    Initial Commit

---
+++ Knapsack/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Knapsack
+//
+//  Created by Elliot Johnston on 12/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
